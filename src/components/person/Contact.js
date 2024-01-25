e6d8a9faf7699ab35162c84adf1f6e96cdc90c04
Boomika <boomikaravi0705@gmail.com>
2024-01-25 10:35:46 +0530

    new

--- src/components/person/Contact.js
+++ src/components/person/Contact.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Contact = () => {
-    return <h1>Mobile Number:</h1>;
-  };
+    return (
+      <div>
+    <h1>Mobile Number:</h1>
+    <p>welcome
+      
+    </p>
+    </div>
+  )}
   
   export default Contact;
